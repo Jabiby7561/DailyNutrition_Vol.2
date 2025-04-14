36ebf91a1aaff324361c0befb289791b6b4278c8
WSPB003 <WSP_B@hotmail.com>
2025-04-14 14:22:42 +0700

    หน้า tdee, bmi + data

---
+++ CalculationRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace DailyNutrition
+{
+    public class CalculationRecord
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Gender { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public float Age { get; set; }
+        public float BMR { get; set; }
+        public float TDEE { get; set; }
+        public string ActivityLevel { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
